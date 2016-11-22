cce1840df992835346c3dde3fdf3ad99032e3c8e
davidachee <david.achee@gmail.com>
2016-11-22 17:14:07 -0600

    Create test

--- test/test.js
+++ test/test.js
@@ -23,132 +23,257 @@ describe('Testing Exports', function() {
         describe('Get Ticket', function() {
             beforeEach(function() {
                 var getResponse = {
-                    id: 2,
-                    ticketType: 'ServiceRequest',
-                    dateCreated: '2016-11-21T20:38:54.000+0000',
-                    dueDate: '2016-11-22T06:15:00.000+0000',
-                    lastStatusChangedDate: '2016-11-21T20:38:54.000+0000',
-                    reservation: {
-                        uuid: '123456',
-                        start: '2016-11-22T06:00:00Z',
-                        end: '2016-11-24T06:00:00Z',
-                        reservationNumber: '78910',
-                        firstname: 'John',
-                        lastname: 'Doe',
-                        phone: '+155555555',
-                        email: 'john@doe.com',
-                        id: 123456,
-                        status: 'Approved'
+                    "approvedComment": "string",
+                    "approver": {
+                        "id": 0,
+                        "name": "string"
                     },
-                    approver: { id: 123456 },
-                    workflowStatus: { id: 123456, name: 'Requested', abbreviation: 'REQ' },
-                    requester: 'Guest'
+                    "dateCreated": "2016-11-22T22:19:37.079Z",
+                    "dueDate": "2016-11-22T22:19:37.079Z",
+                    "facility": {
+                        "id": 0,
+                        "name": "string"
+                    },
+                    "id": 0,
+                    "lastEditedByName": "string",
+                    "lastEditedDate": "2016-11-22T22:19:37.079Z",
+                    "lastStatusChangedDate": "2016-11-22T22:19:37.079Z",
+                    "menuOrder": {
+                        "dateCreated": "2016-11-22T22:19:37.079Z",
+                        "deliveryDateType": "string",
+                        "deliveryTime": "string",
+                        "facilityId": 0,
+                        "facilityName": "string",
+                        "id": 0,
+                        "info": "string",
+                        "items": [{
+                            "comment": "string",
+                            "deliveryTime": "string",
+                            "id": 0,
+                            "name": "string",
+                            "options": [{
+                                "displayValue": "string",
+                                "id": "222",
+                                "name": "string",
+                                "price": 0,
+                                "value": "2020-03-03T12:30:00.000Z"
+                            }],
+                            "price": 0,
+                            "quantity": 0,
+                            "totalPrice": 0
+                        }],
+                        "menuId": 0,
+                        "menuName": "string",
+                        "status": "string",
+                        "totalPrice": 0
+                    },
+                    "owner": {
+                        "id": 0,
+                        "name": "string"
+                    },
+                    "requester": "Guest",
+                    "reservation": {
+                        "address": "string",
+                        "email": "string",
+                        "end": "2016-11-22T22:19:37.081Z",
+                        "firstname": "string",
+                        "id": 0,
+                        "language": "string",
+                        "lastname": "string",
+                        "lateCheckoutTime": "2016-11-22T22:19:37.081Z",
+                        "notes": "string",
+                        "phone": "string",
+                        "prefix": "string",
+                        "reservationNumber": "string",
+                        "roomNumber": "string",
+                        "start": "2016-11-22T22:19:37.081Z",
+                        "status": "New",
+                        "uuid": "string",
+                        "vip": "string"
+                    },
+                    "serviceRequest": {
+                        "dateCreated": "2016-11-22T22:19:37.081Z",
+                        "facilityId": 0,
+                        "facilityName": "string",
+                        "id": 0,
+                        "info": "string",
+                        "options": [{
+                            "displayValue": "string",
+                            "id": "222",
+                            "name": "string",
+                            "value": "2020-03-03T12:30:00.000Z"
+                        }],
+                        "serviceId": 0,
+                        "serviceName": "string",
+                        "status": "string"
+                    },
+                    "ticketType": "MenuOrder",
+                    "workflowStatus": {
+                        "abbreviation": "string",
+                        "id": 0,
+                        "name": "string"
+                    }
                 };
                 nock('http://rest.aliceapp.com/staff/v1')
-                    .get('/hotels/1/tickets/2?apikey=123456')
+                    .get('/hotels/1/tickets/0?apikey=123456')
                     .reply(200, getResponse);
             })
             it('Should get a ticket', function() {
-                return alice.tickets('get', { hotelId: '1', ticketId: '2' }).should.be.fulfilled;
+                return alice.tickets('get', { hotelId: '1', ticketId: '0' }).should.be.fulfilled;
             })
             it('Should have an ID', function() {
-                return alice.tickets('get', { hotelId: '1', ticketId: '2' }).should.eventually.have.property("id");
+                return alice.tickets('get', { hotelId: '1', ticketId: '0' }).should.eventually.have.property("id");
             })
             it('Should have the same ID as the request', function() {
-                return alice.tickets('get', { hotelId: '1', ticketId: '2' })
-                .then(ticket => {
-                    expect(ticket.id).to.equal(2);
-                })
+                return alice.tickets('get', { hotelId: '1', ticketId: '0' })
+                    .then(ticket => {
+                        expect(ticket.id).to.equal(0);
+                    })
             })
         });
+        describe('Create Ticket', function() {
+            beforeEach(function() {
+                var createResponse = {
+                    id: 0
+                };
+                nock('http://rest.aliceapp.com/staff/v1')
+                    .post('/hotels/1/tickets/serviceRequest?apikey=123456')
+                    .reply(200, createResponse);
+            })
+            it('Should require a hotel ID', function() {
+                return alice.tickets('create', {
+                    request: {
+                        "dueDate": "2016-11-22T22:19:37.270Z",
+                        "info": "Bring towels",
+                        "lang": "en",
+                        "options": [{
+                            "id": "222",
+                            "value": "2020-03-03T12:30:00.000Z"
+                        }],
+                        "owner": 0,
+                        "requester": "Guest",
+                        "reservation": 0,
+                        "roomNumber": "string",
+                        "serviceId": "102"
+                    }
+                }).should.be.rejected;
+            })
+            it('Should require a request', function() {
+                return alice.tickets('create', { hotelId: '1' }).should.be.rejected;
+            })
+            it('Should create a service request', function() {
+                return alice.tickets('create', {
+                    hotelId: '1',
+                    request: {
+                        "dueDate": "2016-11-22T22:19:37.270Z",
+                        "info": "Bring towels",
+                        "lang": "en",
+                        "options": [{
+                            "id": "222",
+                            "value": "2020-03-03T12:30:00.000Z"
+                        }],
+                        "owner": 0,
+                        "requester": "Guest",
+                        "reservation": 0,
+                        "roomNumber": "string",
+                        "serviceId": "102"
+                    }
+                }).should.be.fulfilled;
+            })
+        })
         describe('Search Tickets', function() {
             beforeEach(function() {
                 var searchResponse = [{
-                    id: 1,
-                    ticketType: 'ServiceRequest',
-                    serviceRequest: {
-                        id: 1,
-                        serviceId: 1,
-                        serviceName: 'First Service'
+                    "approvedComment": "string",
+                    "approver": {
+                        "id": 0,
+                        "name": "string"
                     },
-                    dateCreated: '2016-05-18T21:38:46.000+0000',
-                    dueDate: '2016-05-20T04:00:00.000+0000',
-                    lastStatusChangedDate: '2016-07-14T14:34:53.000+0000',
-                    lastEditedDate: '2016-07-14T14:34:53.000+0000',
-                    lastEditedByName: 'John Doe',
-                    reservation: {
-                        uuid: '123456',
-                        start: '2016-05-20T04:00:00Z',
-                        end: '2016-05-23T04:00:00Z',
-                        reservationNumber: '123456',
-                        firstname: 'First',
-                        lastname: 'Guest',
-                        language: 'en',
-                        id: 125192,
-                        status: 'Approved'
+                    "dateCreated": "2016-11-22T22:19:37.199Z",
+                    "dueDate": "2016-11-22T22:19:37.199Z",
+                    "facility": {
+                        "id": 0,
+                        "name": "string"
                     },
-                    owner: { id: 1, name: 'Joe Owner' },
-                    approver: { id: 1, name: 'Joe Approver' },
-                    workflowStatus: { id: 1, name: 'Closed', abbreviation: 'DONE' },
-                    requester: 'Guest'
-                }, {
-                    id: 2,
-                    ticketType: 'ServiceRequest',
-                    serviceRequest: {
-                        id: 2,
-                        serviceId: 2,
-                        serviceName: 'Second Service'
+                    "id": 0,
+                    "lastEditedByName": "string",
+                    "lastEditedDate": "2016-11-22T22:19:37.199Z",
+                    "lastStatusChangedDate": "2016-11-22T22:19:37.199Z",
+                    "menuOrder": {
+                        "dateCreated": "2016-11-22T22:19:37.199Z",
+                        "deliveryDateType": "string",
+                        "deliveryTime": "string",
+                        "facilityId": 0,
+                        "facilityName": "string",
+                        "id": 0,
+                        "info": "string",
+                        "items": [{
+                            "comment": "string",
+                            "deliveryTime": "string",
+                            "id": 0,
+                            "name": "string",
+                            "options": [{
+                                "displayValue": "string",
+                                "id": "222",
+                                "name": "string",
+                                "price": 0,
+                                "value": "2020-03-03T12:30:00.000Z"
+                            }],
+                            "price": 0,
+                            "quantity": 0,
+                            "totalPrice": 0
+                        }],
+                        "menuId": 0,
+                        "menuName": "string",
+                        "status": "string",
+                        "totalPrice": 0
                     },
-                    dateCreated: '2016-05-19T20:26:31.000+0000',
-                    dueDate: '2016-05-20T04:00:00.000+0000',
-                    lastStatusChangedDate: '2016-07-14T14:26:03.000+0000',
-                    lastEditedDate: '2016-07-14T14:26:03.000+0000',
-                    lastEditedByName: 'Jane Doe',
-                    reservation: {
-                        uuid: '234567',
-                        start: '2016-05-31T04:00:00Z',
-                        end: '2016-06-01T04:00:00Z',
-                        reservationNumber: '234567',
-                        firstname: 'Second',
-                        lastname: 'Guest',
-                        language: 'en',
-                        id: 2,
-                        status: 'Approved'
+                    "owner": {
+                        "id": 0,
+                        "name": "string"
                     },
-                    owner: { id: 2, name: 'Joe Owner' },
-                    approver: { id: 2, name: 'Joe Approver' },
-                    workflowStatus: { id: 2, name: 'Closed', abbreviation: 'DONE' },
-                    requester: 'Guest'
-                }, {
-                    id: 3,
-                    ticketType: 'ServiceRequest',
-                    serviceRequest: {
-                        id: 3,
-                        serviceId: 3,
-                        serviceName: 'Third Service'
+                    "requester": "Guest",
+                    "reservation": {
+                        "address": "string",
+                        "email": "string",
+                        "end": "2016-11-22T22:19:37.199Z",
+                        "firstname": "string",
+                        "id": 0,
+                        "language": "string",
+                        "lastname": "string",
+                        "lateCheckoutTime": "2016-11-22T22:19:37.199Z",
+                        "notes": "string",
+                        "phone": "string",
+                        "prefix": "string",
+                        "reservationNumber": "string",
+                        "roomNumber": "string",
+                        "start": "2016-11-22T22:19:37.199Z",
+                        "status": "New",
+                        "uuid": "string",
+                        "vip": "string"
                     },
-                    dateCreated: '2016-05-24T13:59:16.000+0000',
-                    dueDate: '2016-05-24T04:00:00.000+0000',
-                    lastStatusChangedDate: '2016-07-14T14:24:39.000+0000',
-                    lastEditedDate: '2016-07-14T14:24:39.000+0000',
-                    lastEditedByName: 'Joe Doe',
-                    reservation: {
-                        uuid: '345678',
-                        start: '2016-04-07T04:00:00Z',
-                        end: '2016-06-30T04:00:00Z',
-                        roomNumber: 'NY',
-                        reservationNumber: '345678',
-                        firstname: 'Third',
-                        lastname: 'Guest',
-                        language: 'en',
-                        id: 123456,
-                        status: 'Approved',
-                        lateCheckoutTime: '2016-05-05T04:00:00.000+0000'
+                    "serviceRequest": {
+                        "dateCreated": "2016-11-22T22:19:37.199Z",
+                        "facilityId": 0,
+                        "facilityName": "string",
+                        "id": 0,
+                        "info": "string",
+                        "options": [{
+                            "displayValue": "string",
+                            "id": "222",
+                            "name": "string",
+                            "value": "2020-03-03T12:30:00.000Z"
+                        }],
+                        "serviceId": 0,
+                        "serviceName": "string",
+                        "status": "string"
                     },
-                    owner: { id: 3, name: 'Joe Owner' },
-                    approver: { id: 3, name: 'Joe Approver' },
-                    workflowStatus: { id: 5678, name: 'Closed', abbreviation: 'DONE' },
-                    requester: 'Guest'
+                    "ticketType": "MenuOrder",
+                    "workflowStatus": {
+                        "abbreviation": "string",
+                        "id": 0,
+                        "name": "string"
+                    }
                 }];
                 nock('http://rest.aliceapp.com/staff/v1')
                     .get('/hotels/1/tickets?ticketTypes=ServiceRequest&apikey=123456')
